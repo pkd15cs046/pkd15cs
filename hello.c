722901d292a2f3f5be3d1471882f237758418500
pkd15cs063 <Ajinshadwdr@gmail.com>
2017-04-12 11:01:05 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main()
 {
+  printf("hello gd mng");
+  printf("*************************");
+  printf("welcome to git program");
 printf("welcome to git programming");
 return 0;
 }
