dc66c42a35167df094717fb9ab27094083c5c629
pkd15cs063 <Ajinshadwdr@gmail.com>
2017-04-12 11:09:54 +0530

    additon.c

---
+++ adduition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("additon of two numbers");
+printf("     **************     ");
+printf("enter first number");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("sum="+c);
+}
